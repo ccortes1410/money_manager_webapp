ab26a70dbd1a62f24d29de205db0cc0c419ff73b
Carlos Cortes <cortespoblete.carlos@gmail.com>
2025-08-13 00:19:48 -0400

    Update #2

--- money_manager_webapp/settings.py
+++ money_manager_webapp/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = "money_manager_webapp.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "money_manager_db",
+        "USER": "root",
+        "PASSWORD": "Cc.198422231",
+        "HOST": "localhost",
+        "PORT": "3306",
     }
 }
 
---
+++ money_manager_webapp/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from .models import User, Income, Expense
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404, render, redirect
+from django.urls import reverse
+from django.views import generic
+from django.contrib.auth import authenticate, login, logout
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def register_user(request):
+    context = {}
+    if request.method == "GET":
+        return render(request, "moneymanager/user_registration_bootstrap.html", context)
+    elif request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        first_name = request.POST.get("first_name")
+        last_name = request.POST.get("last_name")
+        email = request.POST.get("email")
+
+        user = User(username=username, email=email)
+        user.set_password(password)
+        user.save()
+        user_exist = False
+        
+        try:
+            User.objects.get(username=username)
+            user_exist = True
+        except:
+            logger.error("New user")
+        if not user_exist:
+            user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name, email=email, password=password)
+        logger.info(f"User registered: {username}")
+        return HttpResponseRedirect(reverse("login"))
+
+    return render(request, "register.html")
