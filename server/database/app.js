d30a6cf91d2ff4dd9c613dc58bf99058e23af8bb
Carlos <cortespoblete.carlos@gmail.com>
2026-08-03 19:42:09 -0400

    Fixed minor bugs in docker-compose.yaml and app.js from database.

--- server/database/app.js
+++ server/database/app.js
@@ -9,7 +9,7 @@ const requireInternalApiKey = require('./middleware/auth');
 const app = express();
 const port = process.env.PORT || 3030;
 
-app.unsubscribe(cors());
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
